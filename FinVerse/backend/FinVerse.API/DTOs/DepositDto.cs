ac4f3dcb7591dcf0d06b0aad558f7e8212c97680
meghrajrathod <25meghrajrathod@gmail.com>
2026-05-08 03:33:59 +0530

    Remove FinVerse project from monorepo

--- FinVerse/backend/FinVerse.API/DTOs/DepositDto.cs
+++
@@ -1,7 +0,0 @@
-namespace FinVerse.API.DTOs
-{
-    public class DepositDto
-    {
-        public decimal Amount { get; set; }
-    }
-}
--- FinVerse/backend/FinVerse.API/DTOs/TransferDto.cs
+++
@@ -1,9 +0,0 @@
-namespace FinVerse.API.DTOs
-{
-    public class TransferDto
-    {
-        public string ReceiverEmail { get; set; } = string.Empty;
-
-        public decimal Amount { get; set; }
-    }
-}
